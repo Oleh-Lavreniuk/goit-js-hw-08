68403cca2eae24723eedf8129b599caaacbb6dbd
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-07-24 23:15:43 +0300

    add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_TU_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(ev) {
+    localStorage.setItem(LOCAL_TU_KEY, ev.seconds);
+}
+
+setCurrentTime();
+
+function setCurrentTime() {
+    const getSavedTime = localStorage.getItem(LOCAL_TU_KEY);
+    if(getSavedTime) {
+        player.setCurrentTime(getSavedTime);
+    }
+};
