873759b587cdd9cfc7c0511d150dbf647879b0ff
Oleh-Lavreniuk <98620588+Oleh-Lavreniuk@users.noreply.github.com>
2022-07-26 10:09:28 +0000

    Deploying to gh-pages from @ Oleh-Lavreniuk/goit-js-hw-08@fd977dcb1c15552e5d78165ca131d5a55ff2ffc3 🚀

---
+++ 03-feedback.0e184b2f.js
@@ -0,0 +1,2 @@
+!function(){console.log("Hello world"),document.querySelector(".feedback-form").addEventListener("input",(function(t){var o=t.target;e[o.name]=o.value,localStorage.setItem("feedback-form-state",JSON.stringify(e))}));var e={}}();
+//# sourceMappingURL=03-feedback.0e184b2f.js.map
---
+++ 03-feedback.d862704c.js
@@ -0,0 +1,2 @@
+console.log("Hello world");document.querySelector(".feedback-form").addEventListener("input",(function({target:t}){e[t.name]=t.value,localStorage.setItem("feedback-form-state",JSON.stringify(e))}));const e={};
+//# sourceMappingURL=03-feedback.d862704c.js.map
