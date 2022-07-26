7661806ed1bd6a2be064cc33f5cef4e555f660eb
Oleh-Lavreniuk <98620588+Oleh-Lavreniuk@users.noreply.github.com>
2022-07-26 15:33:03 +0000

    Deploying to gh-pages from @ Oleh-Lavreniuk/goit-js-hw-08@4664cc4117939274d935e9980524b7ad46dd895e 🚀

--- 03-feedback.d862704c.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello world");document.querySelector(".feedback-form").addEventListener("input",(function({target:t}){e[t.name]=t.value,localStorage.setItem("feedback-form-state",JSON.stringify(e))}));const e={};
-//# sourceMappingURL=03-feedback.d862704c.js.map
