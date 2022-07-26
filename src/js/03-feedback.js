4664cc4117939274d935e9980524b7ad46dd895e
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-07-26 18:32:02 +0300

    add task-3 to hw-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,52 @@
-// import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
-console.log('Hello world');
+console.log('Hello MENTOR, and thanks for watching!');
 
 const formRef = document.querySelector('.feedback-form');
-// console.log('formRef', formRef.elements);
 
-formRef.addEventListener('input', onFormInput);
-const formData = {};
+formRef.addEventListener('input', throttle(setLocalData, 500));
+const formData = localStorage.getItem('feedback-form-state')
+  ? JSON.parse(localStorage.getItem('feedback-form-state'))
+  : {};
 
-function onFormInput({ target }) {
-  //   const email = formRef.elements.email;
-  //   console.log(email);
-  //   const message = formRef.elements.message;
+formRef.addEventListener('submit', onClickSubmit);
 
+function setLocalData({ target }) {
   formData[target.name] = target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-// 1) Отслеживай на форме событие input, и каждый раз записывай
-// в локальное хранилище объект с полями email и message, в
-// которых сохраняй текущие значения полей формы.Пусть ключом
-// для хранилища будет строка "feedback-form-state".
+function getLocalData() {
+  const localKey = localStorage.getItem('feedback-form-state');
 
-// 2) При загрузке страницы проверяй состояние хранилища, и если
-// там есть сохраненные данные, заполняй ими поля формы.В
-// противном случае поля должны быть пустыми.
+  if (localKey) {
+    const getlocalKey = JSON.parse(localKey);
 
-// 3) При сабмите формы очищай хранилище и поля формы, а также
-// выводи объект с полями email, message и текущими их значениями
-// в консоль.
+    if (getlocalKey.email) {
+      formRef.elements.email.value = getlocalKey.email;
+    }
+    if (getlocalKey.message) {
+      formRef.elements.message.value = getlocalKey.message;
+    }
+  }
+}
+getLocalData();
+
+function onClickSubmit(ev) {
+  ev.preventDefault();
+
+  const email = ev.currentTarget.elements.email.value;
+  const message = ev.currentTarget.elements.message.value;
+
+  if (email === '' || message === '') {
+    return alert('Всі поля мають бути заповнені!!!');
+  }
+
+  console.log(
+    'getLocalKey',
+    JSON.parse(localStorage.getItem('feedback-form-state'))
+  );
+
+  ev.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
